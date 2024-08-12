82971b8ef703927c708a690c3ce43d613f5ad091
Lucie Laporte <lucielefrapper@mbp-de-lucie.home>
2024-08-12 15:24:02 +0200

    remove useless print

--- 01/02.py
+++ 01/02.py
@@ -1,7 +1,6 @@
 with open("input.txt", "r") as fd:
     lines = fd.readlines()
     lines = [line.rstrip() for line in lines]
-print("lines", lines)
 
 letters_digits = [
     "one",
@@ -18,12 +17,8 @@ sum = 0
 
 for line in lines:
     calibration = []
-    print("line", line)
     for i in range(len(line)):
-        print("i", i)
-        print(line[i:])
         substring = line[i:]
-        print("substring de 0", substring[0])
         if substring[0].isdigit():
             calibration.append(substring[0])
         else:
@@ -31,7 +26,6 @@ for line in lines:
                 if substring.startswith(element):
                     calibration.append(str(letters_digits.index(element) + 1))
 
-        print("calibration", calibration)
     calibration_value = calibration[0] + calibration[-1]
 
     sum += int(calibration_value)
